414d64783f63e0b657de5c000dcf5f3096484c2c
Ivan Sopov <sopov.ivan@gmail.com>
2017-06-18 03:06:45 +0300

    TarantoolException to the core, select by space name

--- tarantool-driver/src/main/java/com/sopovs/moradanen/tarantool/TarantoolClient.java
+++ tarantool-driver/src/main/java/com/sopovs/moradanen/tarantool/TarantoolClient.java
@@ -4,12 +4,24 @@ public interface TarantoolClient {
 
 	Result selectAll(int space, int limit, int offset);
 
+	default Result selectAll(String space, int limit, int offset) {
+		return select(space(space), limit, offset);
+	}
+
 	default Result selectAll(int space) {
 		return selectAll(space, Integer.MAX_VALUE);
 	}
 
+	default Result selectAll(String space) {
+		return selectAll(space(space), Integer.MAX_VALUE);
+	}
+
 	default Result selectAll(int space, int limit) {
 		return selectAll(space, limit, 0);
+	}
+
+	default Result selectAll(String space, int limit) {
+		return selectAll(space(space), limit, 0);
 	}
 
 	default int space(String space) {
@@ -25,18 +37,50 @@ public interface TarantoolClient {
 		}
 	}
 
+	Result select(int space, String key, int index, int limit, int offset);
+
+	default Result select(String space, String key, int index, int limit, int offset) {
+		return select(space(space), key, index, limit, offset);
+	}
+
 	default Result select(int space, String key, int index) {
 		return select(space, key, index, Integer.MAX_VALUE, 0);
 	}
 
-	default Result eval(String expression) {
-		return eval(expression, TupleWriter.EMPTY);
+	default Result select(String space, String key, int index) {
+		return select(space(space), key, index, Integer.MAX_VALUE, 0);
+	}
+
+	default Result select(int space, String key, int index, int limit) {
+		return select(space, key, index, limit, 0);
+	}
+
+	default Result select(String space, String key, int index, int limit) {
+		return select(space(space), key, index, limit, 0);
+	}
+
+	Result select(int space, int key, int index, int limit, int offset);
+
+	default Result select(int space, int key, int index) {
+		return select(space, key, index, Integer.MAX_VALUE, 0);
+	}
+
+	default Result select(String space, int key, int index) {
+		return select(space(space), key, index, Integer.MAX_VALUE, 0);
+	}
+
+	default Result select(int space, int key, int index, int limit) {
+		return select(space, key, index, limit, 0);
+	}
+
+	default Result select(String space, int key, int index, int limit) {
+		return select(space(space), key, index, limit, 0);
 	}
 
 	Result eval(String expression, TupleWriter tupleWriter);
 
-	Result select(int space, String key, int index, int limit, int offset);
-
-	Result select(int space, int key, int index, int limit, int offset);
+	default Result eval(String expression) {
+		return eval(expression, TupleWriter.EMPTY);
+	}
 
 }
