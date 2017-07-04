27a5923c286962f9a5bb40f259550512c23a3b3f
Ivan Sopov <sopov.ivan@gmail.com>
2017-07-04 22:53:51 +0300

    another try to fix build matrix

---
+++ install_tarantool.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+curl http://download.tarantool.org/tarantool/1.${TARANTOOL_VERSION}/gpgkey | sudo apt-key add -
+release=`lsb_release -c -s`
+
+# install https download transport for APT
+sudo apt-get -y install apt-transport-https
+
+# append two lines to a list of source repositories
+sudo rm -f /etc/apt/sources.list.d/*tarantool*.list
+sudo tee /etc/apt/sources.list.d/tarantool_1_${TARANTOOL_VERSION}.list <<- EOF
+deb http://download.tarantool.org/tarantool/1.${TARANTOOL_VERSION}/ubuntu/ $release main
+deb-src http://download.tarantool.org/tarantool/1.${TARANTOOL_VERSION}/ubuntu/ $release main
+EOF
+
+# install
+sudo apt-get update
+sudo apt-get -y install tarantool
